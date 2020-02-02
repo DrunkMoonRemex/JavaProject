1cad648e66372b13a9c6abcc11b26f243fa0fcb4
drunk_moon_remax <898843662@qq.com>
2020-02-02 22:00:34 +0800

    commit system

---
+++ src/com/company/Test6System.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Test6System {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+
+        int[] arr = {12,34,56,78};
+        int[] b = new int[5];
+        System.arraycopy(arr,1,b,1,2);//将数组arr下标1的位置，复制到数组b从下标1开始的位置，共复制2个
+        /*
+            src --> 源数组
+　　　　　　srcPos --> 源数组中的起始位置。
+　　　　　　dest --> 目标数组。
+　　　　　　destPos --> 目标数据中的起始位置。
+　　　　　　length --> 要复制的数组元素的数量。
+         */
+        for(int i = 0 ; i < b.length; i++){
+            System.out.println(b[i]);
+        }
+
+        System.out.println(System.currentTimeMillis());
+    }
+}
