f86ded1cc46f377cc40b80459e4c37337938832c
drunk_moon_remax <898843662@qq.com>
2020-02-02 20:06:15 +0800

    增加2个类

--- src/com/company/FloatDateTemplateTest.java
+++ src/com/company/FloatDateTemplateTest.java
@@ -1,14 +1,58 @@
 package com.company;
 
+import java.math.BigDecimal;
+
 //浮点类型测试
 public class FloatDateTemplateTest {
     public static void main(String[] args) {
-        float a = 3.14f;
-        double b = 6.28;
-        double c = 628E-2;
+        float a = 3.14f; //默认是double类型，转成float需要末尾加f或F
+        double b = 6.28;//可加d或D也可不加
+        double c = 628E-2;//科学计数法 表示628乘10的负二次方
 
         System.out.println(a);
         System.out.println(b);
         System.out.println(c);
+
+
+        //浮点数是不精确的，一定不要用于比较
+        float f = 0.1f;
+        double d = 1.0/10;
+        System.out.println(f+"----"+d);
+        System.out.println(f == d); //false
+
+        float d1 = 423423423f;
+        float d2 = d1+1;
+        if (d1==d2){
+            System.out.println(d1+"---"+d2);
+            System.out.println("d1==d2");
+        }else{
+            System.out.println("d1!=d2");
+        }
+
+        //要比较请使用BigDecimal类，该类实现了任意精度的浮点运算
+        BigDecimal bigDecimal = BigDecimal.valueOf(1.0);
+        bigDecimal = bigDecimal.subtract(BigDecimal.valueOf(0.1));//subtract 减法，减0.1
+        bigDecimal = bigDecimal.subtract(BigDecimal.valueOf(0.1));
+        bigDecimal = bigDecimal.subtract(BigDecimal.valueOf(0.1));
+        bigDecimal = bigDecimal.subtract(BigDecimal.valueOf(0.1));
+        bigDecimal = bigDecimal.subtract(BigDecimal.valueOf(0.1));
+        System.out.println(bigDecimal);
+        System.out.println(1.0-0.1-0.1-0.1-0.1-0.1);
+
+        BigDecimal bd = BigDecimal.valueOf(0.1);
+        BigDecimal bd2 = BigDecimal.valueOf(1.0/10.0);
+        System.out.println(bd+"-----"+bd2);
+        System.out.println(bd.equals(bd2));
+
     }
 }
+
+
+
+
+
+
+
+
+
+
