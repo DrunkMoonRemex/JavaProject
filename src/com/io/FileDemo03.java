a3bceef46f287d12b4df9dc136ae79b9e66f2e8f
drunk_moon_remax <898843662@qq.com>
2020-02-04 23:50:08 +0800

    commit io file

--- src/com/io/FileDemo03.java
+++ src/com/io/FileDemo03.java
@@ -29,6 +29,22 @@ public class FileDemo03 {
             2、listFiles() 列出下级File对象
          */
         dir = new File("D:/IdeaProjects/JavaProject");
+        //列出下级名称
+        String[] subNames = dir.list();
+        for (String s : subNames){
+            System.out.println(s);
+        }
 
+        //下级对象
+        File[] filNames = dir.listFiles();
+        for(File f : filNames){
+            System.out.println(f.getAbsolutePath());
+        }
+
+        //列出所有盘符
+        File[] roots = dir.listRoots();
+        for (File f : roots){
+            System.out.println(f.getAbsolutePath());
+        }
     }
 }
