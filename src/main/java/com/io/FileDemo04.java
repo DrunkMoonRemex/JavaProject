f7834e416c83053fcfce3570e4251e51cce7158d
drunk_moon_remax <898843662@qq.com>
2020-02-05 21:06:08 +0800

    commit file

--- src/main/java/com/io/FileDemo04.java
+++ src/main/java/com/io/FileDemo04.java
@@ -1,10 +1,30 @@
 package com.io;
 
+import java.io.File;
+
 /**
  * 递归
  */
 public class FileDemo04 {
     public static void main(String[] args) {
+        File src = new File("D:\\IdeaProjects\\JavaProject\\src\\main\\java");
+        printNames(src,-1);
+    }
+
+    //打印子孙级目录和文件的名称
+    private static void printNames(File src,int deep) {
+        for (int i = 0 ; i < deep ; i++){
+            System.out.print("-");
+        }
+
+        if (null != src && src.exists()) {
+            System.out.println(src.getName());//打印名称
+            if (src.isDirectory()){
+                for (File file : src.listFiles()){
+                    printNames(file,deep+1);
+                }
+            }
+        }
 
     }
 }
