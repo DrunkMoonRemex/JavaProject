b79ab9d3d0af7b2dafb5359313cbdf83e4a1d3b9
drunk_moon_remax <898843662@qq.com>
2020-03-04 21:11:18 +0800

    collection集合

---
+++ src/main/java/com/collection/TestGenerics.java
@@ -0,0 +1,36 @@
+package com.collection;
+
+import java.util.*;
+
+public class TestGenerics {
+    public static void main(String[] args) {
+        // 这里的”String”就是实际传入的数据类型；
+        MyCollection<String> mc = new MyCollection<String>();
+        mc.set("aaa", 0);
+        mc.set("bbb", 1);
+        String str = mc.get(1); //加了泛型，直接返回String类型，不用强制转换;
+        System.out.println(str);
+
+
+        // 以下代码中List、Set、Map、Iterator都是与容器相关的接口;
+        List<String> list = new ArrayList<String>();
+        Set<Man> mans = new HashSet<Man>();
+        Map<Integer, Man> maps = new HashMap<Integer, Man>();
+        Iterator<Man> iterator = mans.iterator();
+    }
+}
+
+class Man{
+
+}
+
+class MyCollection<E> {// E:表示泛型;
+    Object[] objs = new Object[5];
+
+    public E get(int index) {// E:表示泛型;
+        return (E) objs[index];
+    }
+    public void set(E e, int index) {// E:表示泛型;
+        objs[index] = e;
+    }
+}
